7ee92725cd9d22b78ebdabe2cda477ed8136c32f
MacLier <75802094+MacLier@users.noreply.github.com>
2022-08-31 16:16:01 +0200

    feat: adding NewExpense & ExpenseForm components with style

---
+++ 001/src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,28 @@
+import "./ExpenseForm.css"
+
+const ExpenseForm = () => {
+
+    return (
+        <form>
+            <div className="new-expense__controls">
+                <div className="new-expense__control">
+                    <label for="title">Title</label>
+                    <input type="text" id="title" name="title" />
+                </div>
+                <div className="new-expense__control">
+                    <label for="amount">Amount</label>
+                    <input type="number" min="0.01" step="0.01" id="amount" name="amount" />
+                </div>
+                <div className="new-expense__control">
+                    <label for="date">Date</label>
+                    <input type="date" min="2021-09-01" max="2022-12-31" id="date" name="date" />
+                </div>
+            </div>
+            <div className="new-expense__actios">
+                <button type="submit">Add Expense</button>
+            </div>
+        </form>
+    )
+}
+
+export default ExpenseForm;

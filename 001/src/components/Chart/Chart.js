981a283c129f4cac2a70362912bc01163e45998b
MacLier <75802094+MacLier@users.noreply.github.com>
2022-09-02 17:59:18 +0200

    feat: ExpenseChart logic added & Chart logic setted

--- 001/src/components/Chart/Chart.js
+++ 001/src/components/Chart/Chart.js
@@ -4,14 +4,17 @@ import CharBar from "./ChartBar";
 
 const Chart = props => {
 
+    const dataPointsValues = props.dataPoints.map(dataPoint => dataPoint.value);
+    const totalMaximum = Math.max(...dataPointsValues);
+
     return (
         <div className="chart">
-            {props.dataPoints.map(datapoint => {
+            {props.dataPoints.map(dataPoint => {
                 return <CharBar
-                    key={datapoint.label}
-                    value={datapoint.value}
-                    maxValue={null}
-                    label={datapoint.label} />
+                    key={dataPoint.label}
+                    value={dataPoint.value}
+                    maxValue={totalMaximum}
+                    label={dataPoint.label} />
             })}
         </div>
     )

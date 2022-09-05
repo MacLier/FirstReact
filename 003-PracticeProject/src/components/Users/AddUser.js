ed0aae35a9dbadf96e0b6a09354dba0a62cf8d0f
MacLier <75802094+MacLier@users.noreply.github.com>
2022-09-05 18:15:31 +0200

    feat: adding reusable Card component

--- 003-PracticeProject/src/components/Users/AddUser.js
+++ 003-PracticeProject/src/components/Users/AddUser.js
@@ -1,4 +1,6 @@
-import "./AddUser.module.css"
+import classes from "./AddUser.module.css";
+
+import Card from "../UI/Card";
 
 const AddUser = props => {
 
@@ -7,13 +9,15 @@ const AddUser = props => {
 
     }
     return (
-        <form onSubmit={addUserHandler}>
-            <label htmlFor="username">Username</label>
-            <input id="username" type="text" />
-            <label htmlFor="age">Age</label>
-            <input id="age" type="number" />
-            <button type="submit">Add User</button>
-        </form>
+        <Card className={classes.input}>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor="username">Username</label>
+                <input id="username" type="text" />
+                <label htmlFor="age">Age</label>
+                <input id="age" type="number" />
+                <button type="submit">Add User</button>
+            </form>
+        </Card>
     )
 }
 

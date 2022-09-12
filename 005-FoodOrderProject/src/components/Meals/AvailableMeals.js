799695a6a25e024016bc68bda31a0bb5d7dd7f8f
MacLier <75802094+MacLier@users.noreply.github.com>
2022-09-12 18:40:36 +0200

    feat: MealItem & MealItemForm basics added

--- 005-FoodOrderProject/src/components/Meals/AvailableMeals.js
+++ 005-FoodOrderProject/src/components/Meals/AvailableMeals.js
@@ -1,4 +1,6 @@
+import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
+import MealItem from "./MealItem/MealItem";
 
 const DUMMY_MEALS = [
     {
@@ -29,14 +31,22 @@ const DUMMY_MEALS = [
 
 const AvailableMeals = () => {
     const mealsList = DUMMY_MEALS.map(meal => {
-        return <li className={classes.meals}>{meal.name}</li>
+        return <MealItem
+            className={classes.meals}
+            key={meal.id}
+            name={meal.name}
+            description={meal.description}
+            price={meal.price}
+        >{meal.name}</MealItem>
     })
 
     return (
         <section>
-            <ul>
-                {mealsList}
-            </ul>
+            <Card>
+                <ul>
+                    {mealsList}
+                </ul>
+            </Card>
         </section>
     )
 };

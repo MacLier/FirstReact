b1ae02f31c0485f1bf6fd9bbf04008a486be57d8
MacLier <75802094+MacLier@users.noreply.github.com>
2022-09-12 15:49:17 +0200

    feat: HeaderCartButton c & style added

--- 005-FoodOrderProject/src/components/Layout/Header.js
+++ 005-FoodOrderProject/src/components/Layout/Header.js
@@ -1,8 +1,9 @@
+import { Fragment } from "react";
 
 import classes from "./Header.module.css";
 import mealsImage from "../../assets/meals.jpg";
 
-import { Fragment } from "react";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = props => {
 
@@ -11,7 +12,7 @@ const Header = props => {
         <Fragment>
             <header className={classes.header}>
                 <h1>ReactMeals</h1>
-                <button>Cart</button>
+                <HeaderCartButton></HeaderCartButton>
             </header>
             <div className={classes['main-image']}>
                 <img src={mealsImage} alt="A table full of delicious food!" />

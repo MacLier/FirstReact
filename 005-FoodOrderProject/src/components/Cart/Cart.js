9da0f2d5f1558ebb79d7d400f47bbbb17ae1613d
MacLier <75802094+MacLier@users.noreply.github.com>
2022-09-14 17:57:30 +0200

    feat: CartItem added

--- 005-FoodOrderProject/src/components/Cart/Cart.js
+++ 005-FoodOrderProject/src/components/Cart/Cart.js
@@ -2,8 +2,9 @@ import { useContext } from "react";
 
 import classes from "./Cart.module.css";
 
-import Modal from "../UI/Modal";
 import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
+import Modal from "../UI/Modal";
 
 const Cart = props => {
 
@@ -11,10 +12,20 @@ const Cart = props => {
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
     const hasItems = cartCtx.items.length > 0;
 
+    const cartItemRemoveHandler = id => { }
+    const cartItemAddHandler = item => { }
+
     const cartItems = (
         <ul className={classes['cart-items']}>
             {cartCtx.items.map(item =>
-                <li>{item.name}</li>
+                <CartItem
+                    key={item.id}
+                    name={item.name}
+                    amount={item.amount}
+                    price={item.price}
+                    onRemove={cartItemRemoveHandler.bind(null, item.id)}
+                    onAdd={cartItemAddHandler.bind(null, item)}
+                >{item.name}</CartItem>
             )}
         </ul>
     )

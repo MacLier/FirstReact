53330679001cf4fea6da84e39f533f68b73b80e0
MacLier <75802094+MacLier@users.noreply.github.com>
2022-09-19 18:32:17 +0200

    feat: sending POST request

--- 006-http/src/App.js
+++ 006-http/src/App.js
@@ -17,22 +17,24 @@ function App() {
 
     try {
       // const response = await fetch('https://swapi.dev/api/films/'); 
-      const response = await fetch('https://swapi.dev/api/film/'); //changed for error handling
+      const response = await fetch('https://react-http-c672b-default-rtdb.firebaseio.com/movies.json'); //changed for error handling
       if (!response.ok) {
         throw new Error('Something went wrong! Please try again later.');
       }
       const data = await response.json();
 
+      const loadedMovies = [];
 
-      const transformedMovies = data.results.map(movieData => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date
-        }
-      })
-      setMovies(transformedMovies);
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+
+        })
+      }
+      setMovies(loadedMovies);
 
     } catch (error) {
       setError(error.message);
@@ -44,8 +46,20 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  function addMovieHandler(movie) {
-    console.log(movie);
+  async function addMovieHandler(movie) {
+    try {
+
+      const response = await fetch("https://react-http-c672b-default-rtdb.firebaseio.com/movies.json", {
+        method: 'POST',
+        body: JSON.stringify(movie),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      const data = await response.json();
+    } catch (error) {
+      console.log(error.message);
+    }
   }
 
 

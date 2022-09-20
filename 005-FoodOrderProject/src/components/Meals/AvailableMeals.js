f9f41c5d89a8cfbd4c8d25fd36435c94a554f70f
MacLier <75802094+MacLier@users.noreply.github.com>
2022-09-20 16:30:21 +0200

    feat: fetching meals via http

--- 005-FoodOrderProject/src/components/Meals/AvailableMeals.js
+++ 005-FoodOrderProject/src/components/Meals/AvailableMeals.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
@@ -30,7 +31,28 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map(meal => {
+
+    const [meals, setMeals] = useState([]);
+
+    useEffect(() => {
+        const fetchMeals = async () => {
+            const response = await fetch("https://react-http-c672b-default-rtdb.firebaseio.com/meals.json");
+            const responseData = await response.json();
+
+            const loadedMeals = [];
+            for (const key in responseData) {
+                loadedMeals.push({
+                    id: key,
+                    name: responseData[key].name,
+                    description: responseData[key].description,
+                    price: responseData[key].price
+                });
+            }
+            setMeals(loadedMeals);
+        }
+        fetchMeals();
+    }, []);
+    const mealsList = meals.map(meal => {
         return <MealItem
             id={meal.id}
             key={meal.id}
